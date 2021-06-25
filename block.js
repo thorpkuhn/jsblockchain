e1bd07c2f304d8f5f21ea1866a143a489689bbdb
Cryptopragmatist <tristan.alberio@gmail.com>
2021-06-25 09:57:02 +0800

    init

---
+++ block.js
@@ -0,0 +1,62 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp,data, previousHash ='') {
+        this.index= index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash= previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock () {
+        return new Block(0, "01/01/2021", "Genesis Block", "0");
+
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock();
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for( let i =1; i < this.chain.length; i++ ){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain [i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if((currentBlock.previousHash) !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let block1 = new Blockchain();
+block1.addBlock(new Block(1, "01/01/2021", { amount: 4}));
+block1.addBlock(new Block(2, "12/01/2021", { amount: 10}));
+
+console.log('Is blockchain valid? ' + block1.isChainValid());
+
+//console.log(JSON.stringify(block1,null, 4));
